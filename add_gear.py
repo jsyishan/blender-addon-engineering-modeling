ccb536b3d39d12931eb11a0d414395836943c34f
jsyishan <404678173@qq.com>
2017-12-06 19:57:04 +0800

    finish but still a bug remain

--- add_gear.py
+++ add_gear.py
@@ -14,7 +14,7 @@ from bpy.props import (
     FloatProperty, IntProperty
     )
 
-def add_gear(m, z, h):
+def add_gear(m, z, h, ir):
 
     if z < 2:
         return False
@@ -39,12 +39,12 @@ def add_gear(m, z, h):
     vmax = 0.0
     vstep = 1.0
 
-    #set pivot point type and location
+    #Set pivot point type and location
     
     bpy.context.screen.areas[4].spaces[0].pivot_point = 'CURSOR'
     bpy.context.screen.areas[4].spaces[0].cursor_location = (0.0, 0.0, 0.0)
 
-    #draw involute
+    #Draw involute
     bpy.ops.mesh.primitive_xyz_function_surface(
         x_eq = invx,
         y_eq = invy,
@@ -58,17 +58,17 @@ def add_gear(m, z, h):
         range_v_step = vstep
     )
     
-    #enter edit mode
+    #Enter edit mode
     bpy.ops.object.mode_set(mode = 'EDIT')
     
-    #remove doubles
+    #Remove doubles
     bpy.ops.mesh.remove_doubles()
     
-    #init bmesh
+    #Init bmesh
     me = bpy.context.active_object.data
     bm = bmesh.from_edit_mesh(me)
     
-    #select/unselect all vertices and edges
+    #Select/unselect all vertices and edges
     #param Boolean@select to select(True) or unselect(False) all verts and edges
     #return None
     def handle_all_verts_and_edges(select):
@@ -80,8 +80,17 @@ def add_gear(m, z, h):
     
         for edge in edges:
             edge.select = select
+            
+    #Select/unselect all faces
+    #param Boolean@select to select(True) or unselect(False) all faces
+    #return None        
+    def handle_all_faces(select):
+        faces = [e for e in bm.faces]
+
+        for face in faces:
+            face.select = select
     
-    #set cursor to selected
+    #Set cursor to selected
     #param None
     #return None
     def set_cursor_to_selected():
@@ -127,9 +136,15 @@ def add_gear(m, z, h):
         vertices[11].select = True
     
         bm.edges.new((vertices[10], vertices[11]))
+        
+    #Create a new face from all of the vertices
+    handle_all_verts_and_edges(True)
+    
+    bm.faces.new(vertices)
     
     #Duplicate and rotate for a phi degree
     handle_all_verts_and_edges(True)
+    handle_all_faces(True)
 
     bpy.ops.mesh.duplicate()
     
@@ -144,6 +159,8 @@ def add_gear(m, z, h):
         matrix = rot,
         verts = selected_involute
     )
+    
+    handle_all_faces(False)
     
     #Draw a semi-circle between the two buttom vertices of the teeth
     handle_all_verts_and_edges(False)
@@ -168,6 +185,7 @@ def add_gear(m, z, h):
     
     #Duplicate and rotate until a full round
     handle_all_verts_and_edges(True)
+    handle_all_faces(True)
     
     for i in range(z - 1):
         bpy.ops.mesh.duplicate()
@@ -192,11 +210,97 @@ def add_gear(m, z, h):
     if len(bm.verts) == 666:
         bpy.ops.mesh.extrude_region_move(TRANSFORM_OT_translate={"value":(0, 0, h)})
     
+    #Create inner circle and bridge with the base circle(vertices)
+    set_cursor_to_selected()
+    
+    handle_all_verts_and_edges(False)
+    handle_all_faces(False)
+    
+    verts_to_base_radius = list(filter(
+        lambda v:
+            round(sqrt(pow(v.co.x, 2) + pow(v.co.y, 2)), 3) <= round(r, 3),
+        bm.verts))
+        
+    bpy.ops.mesh.primitive_circle_add(
+        vertices = len(verts_to_base_radius) / 2,
+        radius = ir,
+        location = bpy.context.scene.cursor_location
+    )
+    
+    for e in list(filter(lambda v: v.co.z == h, verts_to_base_radius)):
+        e.select = True
+        
+    for edge in bm.edges:
+        if edge.verts[0].select and edge.verts[1].select:
+            edge.select = True
+    
+    bpy.ops.mesh.bridge_edge_loops()
+    
+    #Extrude the inner circle to the bottom and bridge as below
+    handle_all_verts_and_edges(False)
+    handle_all_faces(False)
+    
+    verts_to_inner_circle = list(filter(
+        lambda v:
+            round(sqrt(pow(v.co.x, 2) + pow(v.co.y, 2)), 3) <= round(ir, 3),
+        bm.verts))
+    
+    for e in list(filter(lambda v: v.co.z == h, verts_to_inner_circle)):
+        e.select = True
+        
+    for edge in bm.edges:
+        if edge.verts[0].select and edge.verts[1].select:
+            edge.select = True
+    
+    bpy.ops.mesh.extrude_region_move(TRANSFORM_OT_translate={"value":(0, 0, -h)})
+    
+    for e in list(filter(lambda v: v.co.z == 0, verts_to_base_radius)):
+        e.select = True
+        
+    for edge in bm.edges:
+        if edge.verts[0].select and edge.verts[1].select:
+            edge.select = True
+    
+    bpy.ops.mesh.bridge_edge_loops()
+    
+    #Make normals consistent
+    handle_all_verts_and_edges(True)
+    handle_all_faces(True)
+    
+    bpy.ops.mesh.normals_make_consistent()
+    
+    #Make bevel
+    handle_all_verts_and_edges(False)
+    handle_all_faces(False)
+    
+    count = 0
+    for v in bm.verts:
+        if round(sqrt(pow(v.co.x, 2) + pow(v.co.y, 2)), 4) >= round(R - 0.001, 4):
+            count += 1
+            if count == 3:
+                break
+            v.select = True
+        
+    for edge in bm.edges:
+        if edge.verts[0].select and edge.verts[1].select:
+            edge.select = True
+            
+    bpy.ops.mesh.select_mode(type="EDGE")
+    bpy.ops.mesh.select_similar(type='LENGTH')
+    
+    bpy.ops.mesh.bevel(offset=0.6, segments=10)
     
     
     bmesh.update_edit_mesh(me, True)
     
-    return True
+    #Add EDGE_SPLIT modifier
+    bpy.ops.object.mode_set(mode = 'OBJECT')
+    bpy.ops.object.shade_smooth()
+    bpy.ops.object.modifier_add(type='EDGE_SPLIT')
+    
+    bpy.context.screen.areas[4].spaces[0].cursor_location = (0.0, 0.0, 0.0)
+    
+    return True    
 class AddGear(Operator):
     bl_idname = "mesh.involute_gear"
     bl_label = "Add Involute Gear"
